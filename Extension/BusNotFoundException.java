f0427b966db9f7c96b4cd761ed007b5612f0ac95
ksaurav11 <vermasaurav831@gmail.com>
2022-10-02 18:58:47 +0530

    usecase and exception

---
+++ Extension/BusNotFoundException.java
@@ -0,0 +1,13 @@
+package com.masai.Extension;
+
+public class BusNotFoundException extends Exception{
+	
+	public BusNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	public BusNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
---
+++ Extension/InsufficientTicketException.java
@@ -0,0 +1,15 @@
+package com.masai.Extension;
+
+import java.util.zip.Inflater;
+
+public class InsufficientTicketException extends Exception{
+	public InsufficientTicketException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public InsufficientTicketException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
